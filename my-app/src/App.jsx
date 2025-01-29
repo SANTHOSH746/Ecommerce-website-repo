6d860dc66081cee7c4658c564477c6113a16859c
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-01-29 12:37:12 +0530

    first change

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Login } from './Component/Login'
+import { SignUp } from './Component/SignUp'
+import {Home} from './page/Home'
 
 function App() {
   
@@ -11,7 +13,10 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path = "/Home" element = {<Home/>}/>
         <Route path="/login" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp/>}/>
+
       </Routes>
     </>
   )
