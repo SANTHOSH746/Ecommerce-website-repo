1fce3c47f948a95cdde83d91d2a4f424085c9e9a
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-02-15 11:09:46 +0530

    fc

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,18 +5,21 @@ import { Signup } from './Components/Signup'
 import { Home } from './page/Home'
 import { ProductForm } from './Components/Productform'
 import { Productcardseller } from './Components/productcardforseller'
+import Navbar  from './Component/Navbar';
 
 
 function App() {
  
  return (
     <>
+    <Navbar/>
     <Routes>
+
       <Route path="/" element={<Home/>}/>
       <Route path="/Login" element={<Login/>}/>
       <Route path="/Signup" element={<Signup/>}/>
-   <Route path='/productform' element={<ProductForm/>}/>
-   <Route path='/my-product' element={<Productcardseller/>}/>
+      <Route path='/productform' element={<ProductForm/>}/>
+      <Route path='/my-product' element={<Productcardseller/>}/>
     </Routes>
     </>
     
