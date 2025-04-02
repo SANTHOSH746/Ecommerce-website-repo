b3a8a6dac35d306dde7d057740d9118113626efd
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-04-02 10:14:33 +0530

    rectified my code

--- my-app/src/Components/selectAddress.jsx
+++ my-app/src/Components/selectAddress.jsx
@@ -1,8 +1,7 @@
-// Frontend - selectAddress.jsx
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const SelectAddress = ({ userEmail }) => {
+const SelectAddress = ({ userEmail, onAddressSelect }) => {
   const [addresses, setAddresses] = useState([]);
   const [selectedAddress, setSelectedAddress] = useState("");
 
@@ -10,6 +9,7 @@ const SelectAddress = ({ userEmail }) => {
     axios
       .get(`http://localhost:5000/auth/get-addresses?email=${userEmail}`)
       .then((response) => {
+        console.log("Fetched Addresses:", response.data);
         setAddresses(response.data.addresses);
       })
       .catch((error) => {
@@ -21,17 +21,33 @@ const SelectAddress = ({ userEmail }) => {
     setSelectedAddress(event.target.value);
   };
 
+  const handleConfirm = () => {
+    if (!selectedAddress) {
+      alert("Please select an address!");
+      return;
+    }
+    localStorage.setItem("selectedAddress", selectedAddress);
+    onAddressSelect(selectedAddress); 
+  };
+
   return (
     <div>
       <h2>Select Delivery Address</h2>
-      <select onChange={handleSelect} value={selectedAddress}>
-        <option value="">Select an address</option>
-        {addresses.map((address, index) => (
-          <option key={index} value={JSON.stringify(address)}>
-            {address.address1}, {address.city}, {address.country}
-          </option>
-        ))}
-      </select>
+      {addresses.length > 0 ? (
+        <>
+          <select onChange={handleSelect} value={selectedAddress}>
+            <option value="">Select an address</option>
+            {addresses.map((address) => (
+              <option key={address._id} value={JSON.stringify(address)}>
+                {address.address1}, {address.city}, {address.country}
+              </option>
+            ))}
+          </select>
+          <button onClick={handleConfirm}>Confirm Address</button>
+        </>
+      ) : (
+        <p>No addresses found. Please add an address first.</p>
+      )}
     </div>
   );
 };
